97db8342c8f96a9d21318a044c9d49895dead88a
jud-ie19 <owuorakinyijudith@gmail.com>
2022-04-22 08:28:26 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# tuple.py
+
+first=("blue","black","orange")
+second=("green","red","indigo")
+print("len(first) : ", len(first))
+print("max(first) : ", max(first))
+print("min(first) : ", min(first))
+print("first + second :", first + second)
+print("first * 3 : ", first * 3)
+
